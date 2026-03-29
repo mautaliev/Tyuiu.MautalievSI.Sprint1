d2533aec62534cf21a31c1d5082fb2f79e1a1add
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-03-29 18:58:23 +0500

    Task0 v2

--- Tyuiu.MautalievSI.Sprint1.Task0.V15.Lib/DataService.cs
+++ Tyuiu.MautalievSI.Sprint1.Task0.V15.Lib/DataService.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tyuiu.MautalievSI.Sprint1.Task0.V15.Lib
 {
-    public class DataService
+    public class DataService : ISprint1Task0V15
     {
         public double Calculate()
         {
---
+++ Tyuiu.MautalievSI.Sprint1.Task0.V15.Lib/ISprint1Task0V15.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.MautalievSI.Sprint1.Task0.V15.Lib
+{
+    public interface ISprint1Task0V15
+    {
+        double Calculate();
+    }
+}
