549260e80699c4a1b21e4be682807a7afced030f
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-03-29 20:12:43 +0500

    Task2

---
+++ Tyuiu.MautaliSI.Sprint1.Task2.V19.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MautaliSI.Sprint1.Task2.V19.Lib
+{
+    public class DataService : ISprint1Task2V19
+    {
+        public double ConvertInchToKm(int value)
+        {
+            return value * 0.0254;
+        }
+    }
+}
---
+++ Tyuiu.MautaliSI.Sprint1.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MautaliSI.Sprint1.Task2.V19.Lib;
+
+namespace Tyuiu.MautaliSI.Sprint1.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 10000;
+            var res = ds.ConvertInchToKm(x);
+            Assert.AreEqual(254, res);
+        }
+    }
+}
