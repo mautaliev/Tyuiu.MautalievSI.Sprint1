c967309e0831e3b35ba79f8b613f8e1f6890e64a
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-03-29 18:32:55 +0500

    Add project files.

---
+++ Tyuiu.MautalievSI.Sprint1.Task0.V15.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.MautalievSI.Sprint1.Task0.V15.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.MautalievSI.Sprint1.Task0.V15.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MautalievSI.Sprint1.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
